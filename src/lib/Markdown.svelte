858609c1ac99821f4c56cefe75310030bf8c7174
Richard <richard@stromer.org>
2024-08-26 10:53:29 +0200

    add markdown component

---
+++ src/lib/Markdown.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	const { content }: { content: string } = $props();
+
+	const html = $derived(marked.parse(content));
+</script>
+
+{@html html}
